74ce1eea93fc75ebfe7267022afb3b1ad77fc227
franklee00 <lizehangsz@gmail.com>
2020-11-25 23:19:28 +0800

    Update main.py
    print the output to csv file as running log.

--- FlashSwapForCofixAndUni/main.py
+++ FlashSwapForCofixAndUni/main.py
@@ -1,6 +1,7 @@
 from swapContract import initiateFlashLoan
 from config import *
 import time
+import csv
 
 if __name__ == '__main__':
     while 1:
@@ -12,7 +13,12 @@ if __name__ == '__main__':
             try:
                 tx_dic_gas = w3.eth.estimateGas(tx_dic)
             except ValueError:
-                print("unlucky", time.strftime("%Y--%m--%d %H:%M:%S", timeArray))
+                print("unlucky", time.strftime("%Y-%m-%d %H:%M:%S", timeArray))
+                with open("runninglog.csv","w",newline="") as datacsv:
+                    #dialect为打开csv文件的方式，默认是excel，delimiter="\t"参数指写入的时候的分隔
+                    csvwriter = csv.writer(datacsv,dialect = ("excel"))
+                    #csv文件插入一行数据，把下面列表中的每一项放入一个单元格（可以用循环插入多行）
+                    csvwriter.writerow(["unlucky", time.strftime("%Y-%m-%d %H:%M:%S", timeArray)])
             else:
                 print("success")
                 sendTransationWithMoreGas(tx_dic, "2")
